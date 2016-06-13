e06ba0a41ebcd99c0503353ec65392418fac74e3
desiderata <desiderata2k@gmail.com>
2016-06-13 15:09:02 +0300

    project detail

--- perfect_space/apps/projects/views.py
+++ perfect_space/apps/projects/views.py
@@ -6,8 +6,13 @@ from perfect_space.apps.projects.models import Project
 
 class ProjectDetail(DetailView):
     model = Project
-    context_object_name = 'project'
+    context_object_name = 'page'
     template_name = 'projects/detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['projects_similar'] = Project.objects.all()[:3]
+        return context
 
 
 class ProjectList(ListView):
