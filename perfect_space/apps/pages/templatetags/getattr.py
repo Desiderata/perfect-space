9c0dbc9d206ab40bdf8f780cff2e855125c540b9
desiderata <desiderata2k@gmail.com>
2016-06-14 10:57:08 +0300

    change lang

--- perfect_space/apps/pages/templatetags/getattr.py
+++ perfect_space/apps/pages/templatetags/getattr.py
@@ -1,5 +1,6 @@
 # coding=utf-8
 from django import template
+from django.core.urlresolvers import resolve, reverse
 from django.utils import translation
 from django.utils.safestring import mark_safe
 
@@ -10,3 +11,16 @@ register = template.Library()
 def getattribute(context, obj, attribute):
     lang = translation.get_language()
     return mark_safe(getattr(obj, '{}_{}'.format(attribute, lang)))
+
+
+@register.simple_tag(takes_context=True)
+def change_lang(context, lang=None, *args, **kwargs):
+    """
+    Get active page's url by a specified language
+    Usage: {% change_lang 'en' %}
+    """
+
+    path = context['request'].path
+    url = path.replace('/ru/', '/en/') if lang == 'en' else path.replace('/en/', '/ru/')
+
+    return url
