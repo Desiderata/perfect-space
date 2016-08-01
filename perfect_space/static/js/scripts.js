e6a03c123cfdb07325cbf086b4c21fbbf498fbef
desiderata <desiderata2k@gmail.com>
2016-08-01 17:10:24 +0300

    rework menu

--- perfect_space/static/js/scripts.js
+++ perfect_space/static/js/scripts.js
@@ -32,10 +32,10 @@ $(document).ready(function () {
 });
 
 function fixMenuWidth() {
-    var menuItems = document.getElementById('menu').querySelectorAll('li');
-    for (var i=-1, l=menuItems.length; ++i<l;) {
-        var item = menuItems[i];
-        item.style.width = item.clientWidth + 2 + 'px';
-        item.style.height = item.clientHeight + 'px';
-    }
+    //var menuItems = document.getElementById('menu').querySelectorAll('li');
+    //for (var i=-1, l=menuItems.length; ++i<l;) {
+    //    var item = menuItems[i];
+    //    item.style.width = item.clientWidth + 2 + 'px';
+    //    item.style.height = item.clientHeight + 'px';
+    //}
 }
